b95e80929ce0486c7ed55d5948a98392b220ec2c
tuhoangbk <hoangtu.bk.k58@gmail.com>
2018-05-18 15:25:13 +0700

    Better Structure

--- app/translate.py
+++ app/translate.py
@@ -1,21 +1,19 @@
-"""
- * Created by PyCharm.
- * User: tuhoangbk
- * Date: 17/05/2018
- * Time: 10:38
- * Have a nice day　:*)　:*)
-"""
-from app import app
-from google.cloud import translate as trans
+import json
+import requests
+from flask import current_app
+from flask_babel import _
 
 
-def translate(text, source_language, dest_language = 'vi'):
-    translate_client = trans.Client()
-    translation = translate_client.translate(
-        text,
-        target_language=dest_language)
-
-    # print('Text: {}'.format(text))
-    # print('Translation: {}'.format(translation['translatedText']))
-
-    return translation['translatedText']
+def translate(text, source_language, dest_language):
+    if 'MS_TRANSLATOR_KEY' not in current_app.config or \
+            not current_app.config['MS_TRANSLATOR_KEY']:
+        return _('Error: the translation service is not configured.')
+    auth = {
+        'Ocp-Apim-Subscription-Key': current_app.config['MS_TRANSLATOR_KEY']}
+    r = requests.get('https://api.microsofttranslator.com/v2/Ajax.svc'
+                     '/Translate?text={}&from={}&to={}'.format(
+                         text, source_language, dest_language),
+                     headers=auth)
+    if r.status_code != 200:
+        return _('Error: the translation service failed.')
+    return json.loads(r.content.decode('utf-8-sig'))
